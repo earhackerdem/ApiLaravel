79bb0b10f0d35911d9a5c104e5d63933b0e2891f
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-11-05 17:58:02 -0600

    5 - Configurando rutas

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,10 +38,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('api/v1')
                 ->middleware('api')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->group(base_path('routes/api-v1.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)

cf4f227b876f280a3c1ec178b1f3744b31e0ddbb
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-11-05 18:28:10 -0600

    6 - Registro de usuarios

---
+++ app/Http/Controllers/Api/V1/RegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+
+        ]);
+
+        $user = User::create($request->all());
+
+        return response($user,200);
+
+    }
+}
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('prueba',function(){
-    return "prueba";
-});
+Route::post('register',[RegisterController::class,'store'])->name('api.v1.register');

cbc7ff544615f30731f166571edde9279d26467c
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-12-29 16:03:19 -0600

    21 - Instalar Laravel Passport

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -31,16 +37,19 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'name' => 'required|max:255',
             'slug' => 'required|max:255|unique:posts',
             'extract' => 'required',
             'body' => 'required',
-            'category_id' => 'required|exists:categories,id',
-            'user_id' => 'required|exists:users,id',
+            'category_id' => 'required|exists:categories,id'
         ]);
 
-        $post = Post::create($request->all());
+        $user = auth()->user();
+
+        $data['user_id'] = $user->id;
+
+        $post = Post::create($data);
 
         return PostResource::make($post);
     }

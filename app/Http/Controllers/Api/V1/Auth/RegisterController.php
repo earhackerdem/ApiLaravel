522dd577d88153b816660ef5036a224d803b9054
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-01-03 20:20:01 -0600

    29 - Registrando usuario desde el cliente

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -17,9 +18,12 @@ class RegisterController extends Controller
 
         ]);
 
-        $user = User::create($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
 
-        return response($user,200);
-
+        return UserResource::make($user);
     }
 }
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CategoryController;
-use App\Http\Controllers\Api\V1\RegisterController;
+use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;

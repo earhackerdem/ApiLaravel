38ac41dc351dddb6b1a423aa7b9f78dcd7369ff0
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-01-05 17:10:21 -0600

    36 - Instalar laravel breeze en nuestra api

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+
+    <x-slot name="header">
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Clientes
+        </h2>
+
+    </x-slot>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('clients',[ClientController::class,'index'])->name('clients.index');

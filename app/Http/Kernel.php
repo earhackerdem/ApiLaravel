f1be1c8542cee1151f8f3d3ac4a495fd63cff4eb
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-01-13 17:57:43 -0600

    56 - Proteger rutas por scopes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
+use Laravel\Passport\Http\Middleware\{CheckForAnyScope, CheckScopes};
 class Kernel extends HttpKernel
 {
     /**
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'scopes' => CheckScopes::class,
+        'scope' => CheckForAnyScope::class,
+
     ];
 }
